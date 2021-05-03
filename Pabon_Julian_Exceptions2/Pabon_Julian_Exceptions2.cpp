a84be16ddcaa5fdf060e92765fc51c7f770bcad4
Julian Pabon <julianps101@hotmail.com>
2021-05-03 02:16:02 -0400

    Fixed half function

--- Pabon_Julian_Exceptions2/Pabon_Julian_Exceptions2.cpp
+++ Pabon_Julian_Exceptions2/Pabon_Julian_Exceptions2.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 using namespace std;
+#include <typeinfo>
 //define template
 template <class T>
 //function defenition
@@ -24,6 +25,11 @@ int main()
 template<class T>
 T half(T value)
 {
-	//turn value
+	//test for type double or float
+	if (typeid(value).name() == "double" || typeid(value).name() == "float");
+	{
+		return (value / 2);
+	}
+	//if not double or float, convert into int
 	return static_cast<int>(round((value / 2.0)));
 }
